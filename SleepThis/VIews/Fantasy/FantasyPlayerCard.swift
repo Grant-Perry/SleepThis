99f17c5cfa3df23c21800af0b07e6e53feeb491d
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-12-01 20:23:30 -0500

    v3.07

--- SleepThis/VIews/Fantasy/FantasyPlayerCard.swift
+++ SleepThis/VIews/Fantasy/FantasyPlayerCard.swift
@@ -5,7 +5,7 @@ struct FantasyPlayerCard: View {
    let fantasyViewModel: FantasyMatchupViewModel
    @State private var teamColor: Color = .gray
    @State private var nflPlayer: NFLRosterModel.NFLPlayer?
-
+   
    var body: some View {
 	  ZStack(alignment: .topLeading) {
 		 // Background gradient
@@ -20,7 +20,7 @@ struct FantasyPlayerCard: View {
 			   RoundedRectangle(cornerRadius: 15)
 				  .stroke(Color.gray, lineWidth: 1) // Add 1px gray stroke
 			)
-
+		 
 		 // Team Logo
 		 if let teamLogoURL = getTeamLogoURL() {
 			AsyncImage(url: teamLogoURL) { phase in
@@ -40,12 +40,41 @@ struct FantasyPlayerCard: View {
 			   }
 			}
 			.frame(width: 80, height: 80)
-			.clipShape(RoundedRectangle(cornerRadius: 8))
-			.offset(x: 40, y: -4) // Adjusted offset
+			.offset(x: 20, y: -4) // Adjusted offset
 			.zIndex(0) // Set z-index to be behind player image
-			.opacity(0.8) // Add opacity to the team logo
+			.opacity(0.6) // Add opacity to the team logo
+						  // Add glow effect
+			.shadow(color: teamColor.opacity(0.5), radius: 10, x: 0, y: 0)
 		 }
-
+		 
+		 // MARK: Jersey number #
+		 VStack {
+			//			Spacer()
+			HStack {
+			   Spacer() // Push the ZStack to the trailing edge
+			   ZStack(alignment: .topTrailing) {
+				  // APPLY: Separate jersey number and '#' symbol
+				  Text(nflPlayer?.jersey ?? "")
+					 .font(.system(size: 85, weight: .bold))
+					 .italic()
+					 .foregroundColor(teamColor.adjustBrightness(by: 0.5))
+					 .opacity(0.35)
+				  
+				  //                  // APPLY: Position '#' symbol at the top of the jersey number
+				  //                  Text("#")
+				  //                     .font(.system(size: 85, weight: .bold))
+				  //                     .italic()
+				  //                     .foregroundColor(teamColor.adjustBrightness(by: 0.5))
+				  //                     .opacity(0.35)
+				  //                     .offset(y: -40) // Adjust this value to fine-tune the position
+			   }
+			}
+			.padding(.trailing, 8)
+			Spacer()
+		 }
+		 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+		 .zIndex(1)
+		 
 		 HStack(spacing: 12) {
 			// Player Image
 			AsyncImage(url: getPlayerImageURL()) { phase in
@@ -67,8 +96,8 @@ struct FantasyPlayerCard: View {
 			.frame(width: 95, height: 95)
 			.clipShape(RoundedRectangle(cornerRadius: 15)) // Clip the image
 			.offset(x: -20, y: -5) // Add this line to offset the image
-			.zIndex(1) // Set z-index to be in front of team logo
-
+			.zIndex(2) // Set z-index to be in front of team logo
+			
 			VStack(alignment: .trailing, spacing: 4) {
 			   // Player name taking up entire top row
 			   Text(player.playerPoolEntry.player.fullName)
@@ -76,36 +105,36 @@ struct FantasyPlayerCard: View {
 				  .fontWeight(.bold)
 				  .foregroundColor(.white)
 				  .lineLimit(1)
-				  .minimumScaleFactor(0.5)
+				  .minimumScaleFactor(0.8)
 				  .frame(maxWidth: .infinity, alignment: .trailing)
 				  .padding(.top, 4)
-				  .zIndex(2)
-
+				  .zIndex(3)
+			   
 			   Text(getPositionString())
 				  .font(.system(size: 15))
 				  .foregroundColor(.white.opacity(0.8))
 				  .padding(.top, 4) // Add this line to move the position down
-
+			   
 			   Spacer()
-
+			   
 			   HStack(alignment: .bottom, spacing: 4) {
 				  // Player Score
 				  Text(String(format: "%.1f", getPlayerScore()))
-					 .font(.system(size: 14, weight: .bold))
+					 .font(.system(size: 18, weight: .bold))
 					 .foregroundColor(.white)
 					 .lineLimit(1)
 					 .minimumScaleFactor(0.5)
 					 .scaledToFit()
-
-				  Text("…")
-					 .font(.system(size: 14, weight: .bold))
-					 .foregroundColor(.white.opacity(0.7))
+				  
+				  //                  Text("…")
+				  //                     .font(.system(size: 14, weight: .bold))
+				  //                     .foregroundColor(.white.opacity(0.7))
 			   }
-			   .offset(y: -6)
+			   .offset(y: -9)
 			}
 			.padding(.vertical, 8)
 			.padding(.trailing, 8)
-			.zIndex(2)
+			.zIndex(3)
 		 }
 	  }
 	  .frame(height: 95)
@@ -116,7 +145,7 @@ struct FantasyPlayerCard: View {
 		 teamColor = Color(hex: nflPlayer?.team?.color ?? "008C96")
 	  }
    }
-
+   
    func getPlayerImageURL() -> URL? {
 	  let playerId = String(player.playerPoolEntry.player.id)
 	  if fantasyViewModel.leagueID == AppConstants.ESPNLeagueID[1] {
@@ -125,11 +154,11 @@ struct FantasyPlayerCard: View {
 		 return URL(string: "https://sleepercdn.com/content/nfl/players/thumb/\(playerId).jpg")
 	  }
    }
-
+   
    func getTeamLogoURL() -> URL? {
 	  return URL(string: nflPlayer?.team?.logo ?? "")
    }
-
+   
    func getPlayerScore() -> Double {
 	  if fantasyViewModel.leagueID == AppConstants.ESPNLeagueID[1] {
 		 return fantasyViewModel.getPlayerScore(for: player, week: fantasyViewModel.selectedWeek)
@@ -138,7 +167,7 @@ struct FantasyPlayerCard: View {
 		 return fantasyViewModel.calculateSleeperPlayerScore(playerId: String(player.playerPoolEntry.player.id))
 	  }
    }
-
+   
    func getPositionString() -> String {
 	  return fantasyViewModel.positionString(player.lineupSlotId)
    }
